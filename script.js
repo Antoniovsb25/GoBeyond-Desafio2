3d82c87200f5ed052323c920a6b4b8ca2ab2e509
Antônio Basílio <Antoniovsb25@gmail.com>
2021-09-16 00:35:03 -0300

    alguns css adicionados

--- script.js
+++ script.js
@@ -2,28 +2,28 @@ const form = document.getElementById("formulario");
 let funcionarios = [];
 
 form.addEventListener("submit", (e) => {
-    
   e.preventDefault();
   const nome = document.getElementById("nome").value;
   const salario = document.getElementById("salario").value;
-  const salarioFormatado = parseFloat(salario)
+  const salarioFormatado = parseFloat(salario);
+  const salario2casas = salarioFormatado.toFixed(2);
   const totalVendas = document.getElementById("totalVendas").value;
-  const totalVendasFormatado = parseFloat(totalVendas)
+  const totalVendasFormatado = parseFloat(totalVendas);
+  const totalVendas2casas = totalVendasFormatado.toFixed(2);
 
   function salarioTotal(nome, sal, totalVendas) {
-    let total = sal + (totalVendas * 0.15)
+    let total = (sal + totalVendas * 0.15).toFixed(2);
     return `${nome} receberá um salário total de ${total} reais`;
   }
 
-  console.log(salarioTotal(nome, salarioFormatado, totalVendasFormatado))
-  
+  console.log(salarioTotal(nome, salarioFormatado, totalVendasFormatado));
+
   let funcionarioData = {
-    nome,
-    salario,
-    totalVendas,
+    nome: nome,
+    salario: salario2casas,
+    totalEmVendas: totalVendas2casas,
   };
 
   funcionarios.push(funcionarioData);
-  console.table(funcionarios)
-
+  console.table(funcionarios);
 });
